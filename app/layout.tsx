dbdaa3e820b2e0e22392646b3885455853931e65
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-24 14:40:16 -0500

    Match fonts to LFBI branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Merriweather, Bebas_Neue } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const merriweather = Merriweather({
+  variable: "--font-merriweather",
   subsets: ["latin"],
-});
+  weight: "300"
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const bebasNeue = Bebas_Neue({
+  variable: "--font-bebas-neue",
+  weight: "400"
+})
 
 export const metadata: Metadata = {
   title: "The Postscript Show",
@@ -24,8 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body 
+        className={`${merriweather.variable} ${bebasNeue.variable}`}
       >
         {children}
       </body>
