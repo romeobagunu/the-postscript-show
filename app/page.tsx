e2077c67f04d7676e4ec8ce344b89bd72d42ea42
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-26 00:25:51 -0500

    Add Latest Episodes component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
+import { LatestEpisodes } from "./components/episodes/latest-episodes";
 import { Hero } from "./components/hero";
 
 export default function Home() {
   return (<>
       <Hero />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LatestEpisodes />
+      </Suspense>
     </>);
 }
