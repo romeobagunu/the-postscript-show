bd16a6390b88b5d3cb345fb2622e683553ae8d0d
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-28 16:32:43 -0500

    Rework hero into row with grid

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,17 +3,17 @@ import { Icon } from "./icon"
 export function Hero() {
     return (
         <div>
-            <div className="h-120 w-full">
-                <div className="flex flex-col items-center justify-center size-full">
-                    <div className="flex flex-col items-center gap-8">
-                        <div className="h-60 w-auto">
-                            <Icon />    
-                        </div>
-                        <h1 className="text-5xl text-center w-full">
-                            In-depth theological and ministry conversations <br />
-                            with pastors, Bible scholars, missionaries, and professors.
-                        </h1>
+            <div className="h-96 mt-16 grid grid-cols-12 gap-8 container mx-auto max-w-360">
+                <div className="col-span-4 flex justify-center items-center">
+                    <div className="size-64">
+                        <Icon />
                     </div>
+                </div>
+                <div className="col-span-8 flex flex-col justify-center items-center gap-4">
+                    <h1 className="text-5xl w-full">
+                        In-depth theological and ministry conversations <br />
+                        with pastors, Bible scholars, missionaries, and professors.
+                    </h1>
                 </div>
             </div>
         </div>
