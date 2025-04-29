425589310df771fe8c127eb0454f09379d391fd1
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 16:39:57 -0500

    Increase container max width at Large to 2/3 (up from 1/2)

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { LatestEpisodesSkeleton } from "./components/episodes/latest-episodes.sk
 
 export default function Home() {
   return (
-    <div className="container lg:max-w-1/2 mx-auto">
+    <div className="container lg:max-w-2/3 mx-auto">
       <Hero />
       <div className="mt-8 grid grid-cols-12 gap-8">
         <div className="col-span-8">
