0d2201bd0f405f99c9cbd01f204e7c5e453f7a31
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 16:35:54 -0500

    Temporarily remove Call-to-Action buttons
    The buttons are too large and the colors are too bold; it's too distracting and off-theme. Will have to brainstorm something new.

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,7 +17,7 @@ const navItems = [
 
 export function Navbar() {
     return (
-        <nav className="fixed inset-x-0 top-0 z-10 h-24 flex items-center justify-between p-2">
+        <nav className="container mx-auto max-w-9/12 h-24 flex items-center justify-between p-2">
             <div className="h-full flex items-center">
                 <div className="h-full mx-6">
                     <Link
@@ -38,7 +38,7 @@ export function Navbar() {
                     ))}
                 </ul> */}
             </div>
-            <ul className="flex gap-4">
+            {/* <ul className="flex gap-4">
                 <li key="spotify">
                     <a href="https://open.spotify.com/show/3sWcPlzlLIZQhjTOnwELpo" target="_blank">
                         <div className="flex gap-2 rounded-xl bg-green-500 w-auto px-3 py-1.5">
@@ -55,7 +55,7 @@ export function Navbar() {
                         </div>
                     </a>
                 </li>
-            </ul>
+            </ul> */}
         </nav>
     )
 }
