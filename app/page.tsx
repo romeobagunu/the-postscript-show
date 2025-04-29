a1aba0e74ae9a876a977a45f9ea7b89a9dd89258
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 16:37:26 -0500

    Enforce Container aross the root Page

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,18 @@ import { Host } from "./components/host";
 import { LatestEpisodesSkeleton } from "./components/episodes/latest-episodes.skeleton";
 
 export default function Home() {
-  return (<>
-          <Hero />
-          <div className="grid grid-cols-12 gap-8 container mx-auto max-w-360">
-            <div className="col-span-8">
-              <Suspense fallback={<LatestEpisodesSkeleton />}>
-                <LatestEpisodes />
-              </Suspense>
-            </div>
-            <div className="col-span-4">
-              <Host />
-            </div>
-          </div>
-    </>);
+  return (
+    <div className="container lg:max-w-1/2 mx-auto">
+      <Hero />
+      <div className="mt-8 grid grid-cols-12 gap-8">
+        <div className="col-span-8">
+          <Suspense fallback={<LatestEpisodesSkeleton />}>
+            <LatestEpisodes />
+          </Suspense>
+        </div>
+        <div className="col-span-4">
+          <Host />
+        </div>
+      </div>
+    </div>);
 }
