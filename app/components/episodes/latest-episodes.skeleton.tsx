12db683cec313d6fa0d3f2a7453f92655191e7ea
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-30 00:22:35 -0500

    Update LatestEpisodes section Skeleton to match

--- app/components/episodes/latest-episodes.skeleton.tsx
+++ app/components/episodes/latest-episodes.skeleton.tsx
@@ -1,33 +1,89 @@
 export function LatestEpisodesSkeleton() {
-    const placeholders = [1,2,3,4,5,6,7,8,9,10]
+    
     return (
-            <div className="max-w-220">
-                <h2 className="text-center text-xl uppercase text-gray-700">Latest Episodes</h2>
-                <ol className="flex-col align-middle justify-center">
-                {placeholders.map((p) => (
-                    <li key={p}
-                        className="flex gap-2 border-b border-gray-200 py-4"
-                    >
-                        <div className="min-w-48 flex justify-center align-middle">
-                            <div className="h-[150px] aspect-square p-2">
-                                <div className="size-full bg-gray-200 rounded-2xl"></div>
+            <div className="bg-gray-800">
+                <div className="h-144 grid grid-cols-12 bg-white rounded-2xl m-8">
+                    <div className="col-span-4 flex justify-center items-center">
+                        <div className="h-6/10 aspect-square rounded-2xl overflow-hidden">
+                            <div className="size-full bg-gray-200"></div>
+                        </div>
+                    </div>
+                    <div className="col-span-8 flex flex-col justify-center">
+                        <div className="py-2 flex gap-2 items-center">
+                            <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                            <span>&middot;</span>
+                            <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                            <span>&middot;</span>
+                            <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                        </div>
+                        <div className="my-2 h-5 w-150 bg-gray-400 rounded-xl"></div>
+                        <div className="my-1 h-2 w-172 bg-gray-200 rounded-xl"></div>
+                        <div className="my-1 h-2 w-172 bg-gray-200 rounded-xl"></div>
+                        <div className="my-2 py-1 px-4 bg-fuchsia-500 rounded-xl w-24"></div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-12 m-8 gap-4">
+                    <div className="col-span-4">
+                        <div className="py-4 px-2 bg-white rounded-2xl shadow-xl grid grid-cols-12">
+                            <div className="col-span-4 flex justify-center align-middle">
+                                <div className="size-[150px] p-2">
+                                    <div className="size-full rounded-2xl bg-gray-200"></div>
+                                </div>
+                            </div>
+                            <div className="col-span-8 flex flex-col justify-center max-w-168 max-h-44">
+                                <div className="flex gap-2 items-center text-gray-400 uppercase text-sm">
+                                    <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                                    <span>&middot;</span>
+                                    <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                                </div>
+                                <div className="my-2 h-5 w-56 bg-gray-400 rounded-xl"></div>
+                                <div className="my-1 h-2 w-85 bg-gray-200 rounded-xl"></div>
+                                <div className="my-1 h-2 w-85 bg-gray-200 rounded-xl"></div>
+                                <div className="my-1 h-2 w-12 bg-sky-200 rounded-xl"></div>
                             </div>
                         </div>
-                        <div className="flex flex-col justify-center max-w-168 max-h-44">
-                            <div className="flex gap-2 text-gray-400 uppercase text-sm">
-                                <div className="h-4 w-16 bg-gray-200 rounded-xl"></div>
-                                <span>&middot;</span>
-                                <div className="h-4 w-16 bg-gray-200 rounded-xl"></div>
+                    </div>
+                    <div className="col-span-4">
+                        <div className="py-4 px-2 bg-white rounded-2xl shadow-xl grid grid-cols-12">
+                            <div className="col-span-4 flex justify-center align-middle">
+                                <div className="size-[150px] p-2">
+                                    <div className="size-full rounded-2xl bg-gray-200"></div>
+                                </div>
                             </div>
-                            <div className="my-2 h-6 w-48 bg-gray-400 rounded-xl"></div>
-                            <div className="my-0.5 h-2 w-156 bg-gray-200 rounded-xl"></div>
-                            <div className="my-0.5 h-2 w-156 bg-gray-200 rounded-xl"></div>
-                            <div className="my-0.5 h-2 w-156 bg-gray-200 rounded-xl"></div>
-                            <div className="my-1 h-2 w-12 bg-sky-200 rounded-xl"></div>
+                            <div className="col-span-8 flex flex-col justify-center max-w-168 max-h-44">
+                                <div className="flex gap-2 items-center text-gray-400 uppercase text-sm">
+                                    <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                                    <span>&middot;</span>
+                                    <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                                </div>
+                                <div className="my-2 h-5 w-56 bg-gray-400 rounded-xl"></div>
+                                <div className="my-1 h-2 w-85 bg-gray-200 rounded-xl"></div>
+                                <div className="my-1 h-2 w-85 bg-gray-200 rounded-xl"></div>
+                                <div className="my-1 h-2 w-12 bg-sky-200 rounded-xl"></div>
+                            </div>
                         </div>
-                    </li>
-                ))}
-            </ol>
+                    </div>
+                    <div className="col-span-4">
+                        <div className="py-4 px-2 bg-white rounded-2xl shadow-xl grid grid-cols-12">
+                            <div className="col-span-4 flex justify-center align-middle">
+                                <div className="size-[150px] p-2">
+                                    <div className="size-full rounded-2xl bg-gray-200"></div>
+                                </div>
+                            </div>
+                            <div className="col-span-8 flex flex-col justify-center max-w-168 max-h-44">
+                                <div className="flex gap-2 items-center text-gray-400 uppercase text-sm">
+                                    <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                                    <span>&middot;</span>
+                                    <div className="h-3 w-14 bg-gray-200 rounded-xl"></div>
+                                </div>
+                                <div className="my-2 h-5 w-56 bg-gray-400 rounded-xl"></div>
+                                <div className="my-1 h-2 w-85 bg-gray-200 rounded-xl"></div>
+                                <div className="my-1 h-2 w-85 bg-gray-200 rounded-xl"></div>
+                                <div className="my-1 h-2 w-12 bg-sky-200 rounded-xl"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
     )
 }
