3f5f42b02a982e78c02eaf92b5389e1a0789636a
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 23:57:49 -0500

    Overhaul Episode listing as hierarchical Tiles

---
+++ app/components/episodes/featured-episode.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+import { Episode } from "@/app/types/episode";
+import { toAbbreviatedDate, toMinutes } from "@/app/lib/episodeHelpers";
+
+import styles from './featured-episode.module.css';
+
+export function FeaturedEpisode({ episode }: {episode: Episode}) {
+    return (
+            <div className="grid grid-cols-12 bg-white rounded-2xl m-8">
+                <div className="col-span-4 flex justify-center items-center">
+                    <a href={episode.link} className="block h-6/10 aspect-square hover:cursor-pointer hover:brightness-50 rounded-2xl overflow-hidden">
+                        <Image
+                            alt={`Episode image for ${episode.title}`}
+                            src={episode.image}
+                            height="3000"
+                            width="3000"
+                            className="size-full"
+                        />
+                    </a>
+                </div>
+                <div className="col-span-8 flex flex-col justify-center">
+                    <div className="py-2 flex gap-2 text-gray-400 uppercase text-xl">
+                        <p>Latest Episode</p>
+                        <span>&middot;</span>
+                        <p>{toAbbreviatedDate(episode.pubDate)}</p>
+                        <span>&middot;</span>
+                        <p>{toMinutes(episode.length)}</p>
+                    </div>
+                    <a href={episode.link}>
+                        <h3 className="font-condensed uppercase hover:underline text-5xl">{episode.title}</h3>
+                    </a>
+                    <div
+                        dangerouslySetInnerHTML={{ __html: episode.description}}
+                        className={styles.descriptionContainer + " py-2 w-4/5 text-gray-500"}
+                    ></div>
+                    <div className="my-2">
+                        <a 
+                            href={episode.link}
+                            className="py-1 px-4 bg-fuchsia-500 text-white rounded-xl w-24 text-center"
+                        >
+                            Listen Now
+                        </a>
+                    </div>
+                </div>
+            </div>
+    )
+}
--- app/components/episodes/latest-episodes.tsx
+++ app/components/episodes/latest-episodes.tsx
@@ -1,14 +1,17 @@
 import { use } from "react";
 import { getEpisodes } from "../../actions";
 import { EpisodeList } from "./episode-list";
+import { FeaturedEpisode } from "./featured-episode";
 
 export function LatestEpisodes() {
     const episodes = use(getEpisodes());
+    const mostRecentEpisode = episodes[0]
+    const nextLatestEpisodes = episodes.slice(1,4)
 
     return (
-        <div className="max-w-220">
-            <h2 className="font-condensed text-center text-xl uppercase text-gray-700">Latest Episodes</h2>
-            <EpisodeList episodes={episodes} />
+        <div className="bg-gray-800">
+            <FeaturedEpisode episode={mostRecentEpisode} />
+            <EpisodeList episodes={nextLatestEpisodes} />
         </div>
     )
 }
