2dd57e5aab4fc92bee066a8db263922aae5c46b4
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-30 01:42:39 -0500

    Add About component

---
+++ app/components/about.tsx
@@ -0,0 +1,41 @@
+import { Interview } from "./interview";
+import { SiSpotify, SiYoutube } from "react-icons/si"
+
+export function About() {
+    return (
+        <div className="h-172 bg-gray-900 grid grid-cols-12">
+            <div className="col-start-2 col-span-3 h-full flex flex-col gap-8 justify-center text-white ">
+                    <h1 className="font-serif italic text-3xl">
+                        <span>The Postscript is a podcast and YouTube series that invites listeners into </span>
+                        <span className="">in-depth theological and ministry conversations</span>
+                        <span> with </span><span className="">pastors, Bible scholars, missionaries, and professors.</span>
+                    </h1>
+                    <div className="flex gap-4">
+                        <a 
+                            className="block border border-white rounded-2xl py-1 px-2 hover:border-green-500 hover:text-green-500"
+                            href="https://open.spotify.com/show/3sWcPlzlLIZQhjTOnwELpo"
+                        >
+                            <div className="flex gap-2 items-center">
+                                <SiSpotify className="inline-block"/>
+                                <span>Follow on Spotify</span>
+                            </div>
+                        </a>
+                        <a 
+                            className="block border border-white rounded-2xl py-1 px-2 hover:border-red-500 hover:text-red-500"
+                            href="https://www.youtube.com/@thepostscriptshow"
+                        >
+                            <div className="flex gap-2 items-center">
+                                <SiYoutube className="inline-block"/>
+                                <span>Subscribe on YouTube</span>
+                            </div>
+                        </a>
+                    </div>
+            </div>
+            <div className="col-start-6 col-span-6 h-full flex justify-center items-center">
+                <div className="w-full aspect-video rounded-2xl overflow-hidden">
+                    <Interview />
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ app/components/interview.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import interview from '@/public/hero-bg-interview.png'
+
+export function Interview() {
+    return (
+        <div className="w-full aspect-video relative">
+            <div className="h-full aspect-video rounded-2xl overflow-hidden">
+                <Image
+                    src={interview}
+                    alt="An interview from the Postscript"
+                    fill
+                    objectFit='cover'
+                    objectPosition='0% 15%'
+                    priority
+                    className="brightness-115"
+                />
+            </div>
+            <div className="absolute top-0 h-1/3 w-full bg-gradient-to-b from-amber-200/40 to-transparent"></div>
+            <div className="absolute bottom-0 h-1/2 w-full  bg-gradient-to-t from-fuchsia-500/60 to-transparent"></div>
+        </div>
+    )
+}
