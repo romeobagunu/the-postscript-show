a642833d522d71d0212cc00eef2cad53e9a559ed
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 23:28:50 -0500

    Add type for Episode

---
+++ app/types/episode.ts
@@ -0,0 +1,11 @@
+export interface Episode {
+    title: string;
+    description: string;
+    date: Date;
+    image: string;
+    episode: number;
+    pubDate: Date;
+    length: number;
+    mp3: string;
+    link: string;
+}
