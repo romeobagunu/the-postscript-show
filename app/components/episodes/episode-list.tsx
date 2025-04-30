91674ab63d837e6a7de48e892b02108eccf48840
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-30 00:22:01 -0500

    Remove unnecessary formatting from EpisodeListItem wrapper

--- app/components/episodes/episode-list.tsx
+++ app/components/episodes/episode-list.tsx
@@ -7,7 +7,7 @@ export function EpisodeList({episodes}: {episodes: Episode[]}) {
         <ol className="grid grid-cols-12 m-8 gap-4">
                 {episodes.map(e => (
                     <li key={e.episode}
-                        className="col-span-4 flex gap-2"
+                        className="col-span-4"
                     >
                         <EpisodeListItem episode={e} />
                     </li>
