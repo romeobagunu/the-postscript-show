ffb8b4d0fc2d67244b0aab1903b1549514c4030b
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-30 01:42:16 -0500

    Implement preliminary Category filtering

--- app/actions.ts
+++ app/actions.ts
@@ -3,8 +3,10 @@
 import { X2jOptions, XMLParser } from "fast-xml-parser";
 
 import { Episode } from "./types/episode";
+import { Category } from "./types/category";
 
-export async function getEpisodes(page: number = 0, pageSize: number = 10): Promise<Episode[]> {
+export async function getEpisodes(page: number = 0, pageSize: number = 10, category: Category | null = null): Promise<Episode[]> {
+
     const res = await fetch("https://feeds.transistor.fm/the-postscript-show");
     const xml = await res.text();
 
@@ -19,9 +21,22 @@ export async function getEpisodes(page: number = 0, pageSize: number = 10): Prom
     const parser = new XMLParser(options);
     const parsed = parser.parse(xml);
 
-    const items = parsed.rss.channel.item.slice(
+    const filtered = parsed.rss.channel.item.filter((rssItem: any) => {
+        switch (category) {
+            case Category.Postscript:
+                return rssItem.title.startsWith("Episode")
+            case Category.Shorts:
+                return rssItem.title.startsWith("Short")
+            case Category.PsPlus:
+                return !rssItem.title.startsWith("Episode") && !rssItem.title.startsWith("Short")
+            default:
+                return true;
+        }
+    })
+
+    const items = filtered.slice(
         0, 
-        pageSize - 1
+        pageSize
     );
 
     const mapped = items.map((i: any) => ({
@@ -34,7 +49,14 @@ export async function getEpisodes(page: number = 0, pageSize: number = 10): Prom
         length: i.itunes_duration,
         mp3: i.enclosure.url,
         link: i.link,
+        category: determineCategory(i)
     }));
 
     return mapped;
+}
+
+function determineCategory(rssItem: any): Category {
+    if (rssItem.title.startsWith("Episode")) return Category.Postscript;
+    if (rssItem.title.startsWith("Short")) return Category.Shorts;
+    return Category.PsPlus;
 }
