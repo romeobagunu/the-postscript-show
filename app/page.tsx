3087b269956b5662b04c9c101c7bc8521d5277cd
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 23:32:23 -0500

    Rework sizing and width on Home

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,19 @@ import { LatestEpisodesSkeleton } from "./components/episodes/latest-episodes.sk
 
 export default function Home() {
   return (
-    <div className="container lg:max-w-2/3 mx-auto">
-      <Hero />
-      <div className="mt-8 grid grid-cols-12 gap-8">
-        <div className="col-span-8">
+    <>
+      <div className="h-[66vh] w-full">
+        <Hero />
+      </div>
+      <div className="h-[66vh] w-full">
           <Suspense fallback={<LatestEpisodesSkeleton />}>
             <LatestEpisodes />
           </Suspense>
-        </div>
+      </div>
+      <div className="container grid grid-cols-12">
         <div className="col-span-4">
           <Host />
         </div>
       </div>
-    </div>);
+    </>);
 }
