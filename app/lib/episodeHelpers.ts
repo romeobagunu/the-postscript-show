70d247dbfeb508ad28da8028523de9d867a4075e
Romeo Bagunu <romeo.bagunu@outlook.com>
2025-04-29 23:29:06 -0500

    Add util functions for displaying Episode data

---
+++ app/lib/episodeHelpers.ts
@@ -0,0 +1,15 @@
+
+const abbreviatedMonths: string[] = [
+    'JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC'
+]
+export function toAbbreviatedMonth(date: Date): string { 
+    return abbreviatedMonths[date.getMonth() - 1];
+}
+
+export function toAbbreviatedDate(date: Date): string {
+    return `${toAbbreviatedMonth(date)} ${date.getDate().toString()}`;
+};
+
+export function toMinutes(lengthInSeconds: number): string { 
+    return `${Math.floor(lengthInSeconds / 60).toString()} MIN`;
+}
